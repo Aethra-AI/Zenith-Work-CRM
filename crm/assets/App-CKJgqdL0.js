96ae6d5c33df7555ec77db6615d8beed89570bb6
Aethra-AI <kxavier011315@gmail.com>
2025-09-17 02:02:41 -0600

    Mensaje descriptivo de los cambios

---
+++ crm/assets/App-CKJgqdL0.js
@@ -0,0 +1,1 @@
+import{j as t,A as r,O as s}from"./index-BVM79_c9.js";const o=()=>t.jsx(r,{children:t.jsx(s,{})});export{o as default};
--- crm/dist/assets/App-N1nIPDWI.js
+++
@@ -1,1 +0,0 @@
-import{j as t,A as r,O as s}from"./index-B0LcFNMA.js";const o=()=>t.jsx(r,{children:t.jsx(s,{})});export{o as default};
